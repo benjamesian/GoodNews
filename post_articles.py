84722ac8923518486119eaa88fcc3425a4eb3b5d
Ben Cook <808@holbertonschool.com>
2020-03-07 12:01:26 -0800

    fix script so function can be imported from elsewhere

--- post_articles.py
+++ post_articles.py
@@ -1,40 +1,34 @@
 #!/usr/bin/env python3
-from datetime import datetime
+"""Function for posting articles to site."""
 import json
 import requests
-from sys import argv
 
-LOGIN_DATA = {
-    'username': argv[1],
-    'password': argv[2]
-}
 URL_LOGIN = 'http://localhost:8000/admin/'
 URL_ENDPOINT = 'http://localhost:8000/post_articles/'
 DATA_FILE = 'articles.json'
 
 
-def add_articles(articles, login=LOGIN_DATA):
-    with requests.Session() as s:
-        s.get(URL_LOGIN)
-        s.headers.update({'X-CSRFToken': s.cookies.get('csrftoken')})
-
-        login_data = dict(username=login.get('username'),
-                          password=login.get('password'),
-                          next='/')
-        r = s.post(URL_LOGIN, data=login_data,
-                   headers=dict(Referer=URL_LOGIN))
-        s.headers.update({
-            'content-type': 'application/json',
-            'X-CSRFToken': s.cookies.get('csrftoken')
+def add_articles(articles, username, password):
+    """Post articles to api endpoint so that can be added to db."""
+    with requests.Session() as session:
+        session.get(URL_LOGIN)
+        session.headers.update({
+            'X-CSRFToken': session.cookies.get('csrftoken')
         })
 
-        r = s.post(
-            URL_ENDPOINT,
-            data=json.dumps(articles)
-        )
+        login_data = dict(username=username,
+                          password=password,
+                          next='/')
+        session.post(URL_LOGIN, data=login_data,
+                     headers=dict(Referer=URL_LOGIN))
+        session.headers.update({
+            'content-type': 'application/json',
+            'X-CSRFToken': session.cookies.get('csrftoken')
+        })
+
+        session.post(URL_ENDPOINT, data=json.dumps(articles))
 
 
 if __name__ == '__main__':
     with open(DATA_FILE, 'r') as f:
-        articles = json.load(f)
-        add_articles(articles)
+        add_articles(json.load(f), 'some_user', 'some_pass')
