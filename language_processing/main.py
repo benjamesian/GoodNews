55584d45d5be0fb8ceac7922396c209b54dc6995
Ben Cook <808@holbertonschool.com>
2020-03-07 21:49:05 -0800

    wip setting up language processing service

--- language_processing/main.py
+++ language_processing/main.py
@@ -3,17 +3,17 @@ from language_processing.ibmcloud import get_sentiments
 from multiprocessing import Pool
 import os
 import requests
+import socket
 
 
 URL_LOGIN = 'http://localhost:8000/admin/'
 URL_ENDPOINT = 'http://localhost:8000/'
-DATA_FILE = 'articles.json'
 
 USERNAME = os.getenv('GOOD_NEWS_USERNAME')
 PASSWORD = os.getenv('GOOD_NEWS_PASSWORD')
 
 
-def add_articles(articles, username, password):
+def add_articles(articles):
     """Post articles to api endpoint so that can be added to db."""
     with requests.Session() as session:
         session.get(URL_LOGIN)
@@ -21,8 +21,8 @@ def add_articles(articles, username, password):
             'X-CSRFToken': session.cookies.get('csrftoken')
         })
 
-        login_data = dict(username=username,
-                          password=password,
+        login_data = dict(username=USERNAME,
+                          password=PASSWORD,
                           next='/')
         session.post(URL_LOGIN, data=login_data,
                      headers=dict(Referer=URL_LOGIN))
@@ -40,15 +40,66 @@ def get_batch_sentiments(articles):
         return pool.map(get_sentiments, articles)
 
 
-def application(environ, start_response):
-    """WSGI server application"""
-    content = json.load(environ['wsgi.input'])
-    articles = get_batch_sentiments(content.get('data'))
-    add_articles(articles, USERNAME, PASSWORD)
+def process_articles(articles):
+    article_titles = map(lambda x: x.get('title'), articles)
+    raw_sentiments = get_batch_sentiments(article_titles)
+    raw_sentiments = [
+        sent.get('document_tone').get('tones')
+        for sent in raw_sentiments
+    ]
+    articles_data = {
+        'articles': [
+            {
+                'article_data': article,
+                'sentiments': [
+                    {
+                        'name': sentiment.get('tone_id'),
+                        'magnitude': sentiment.get('score')
+                    }
+                    for sentiment in sentiments
+                ]
+            }
+            for article, sentiments in zip(articles, raw_sentiments)]
+    }
+    add_articles(articles_data)
 
-    body = {'message': 'data posted'}
-    status = '200 OK'
-    headers = [('Content-Type', 'application/json'),
-               ('Content-Length', str(len(body)))]
-    start_response(status, headers)
-    return json.dumps(body)
+# old wsgi when considering gunicorn
+# def application(environ, start_response):
+#     """WSGI server application"""
+#     content = json.load(environ['wsgi.input'])
+#     articles = get_batch_sentiments(content.get('data'))
+#     add_articles(articles, USERNAME, PASSWORD)
+
+#     body = {'message': 'data posted'}
+#     status = '200 OK'
+#     headers = [('Content-Type', 'application/json'),
+#                ('Content-Length', str(len(body)))]
+#     start_response(status, headers)
+#     return [json.dumps(body)]
+
+
+if __name__ == "__main__":
+    server_address = './uds_socket'
+
+    try:
+        os.unlink(server_address)
+    except OSError:
+        if os.path.exists(server_address):
+            raise
+
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+        sock.bind(server_address)
+        sock.listen(1)
+
+        while True:
+            connection, client_address = sock.accept()
+            try:
+                data = b''
+                while True:
+                    new_data = connection.recv(1024)
+                    data += new_data
+                    if not new_data:
+                        break
+                process_articles(json.load(data))
+            finally:
+                connection.close()
