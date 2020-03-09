37b92e399252b6ab599cbd1ab1dcac165aaa9391
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-03-08 18:33:14 -0700

    Removed debugging print statement

--- scraping/core/main.py
+++ scraping/core/main.py
@@ -13,7 +13,6 @@ def main():
     """
     for client in (cls() for cls in scraping.API_CLIENTS):
         if 200 <= client.request() < 300:
-            print(client.results())
             with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
                 sock.connect(scraping.SOCKET_PATH)
                 sock.sendall(json.dumps(client.results()).encode())
