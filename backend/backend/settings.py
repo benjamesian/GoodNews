8efe4a07695f3dd60e8fea050bdb921269d56879
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-03-11 16:00:43 -0700

    Redirected Django static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,4 +128,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = '/home/ubuntu/GoodNews/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'www/static/')
