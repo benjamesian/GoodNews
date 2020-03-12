06bc657760c6903ceb7d8e34185a53f45ffc9580
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-03-11 23:32:11 -0700

    Adding logging

--- scraping/__init__.py
+++ scraping/__init__.py
@@ -2,7 +2,14 @@
 """
 Provides tools to collect news articles from the web
 """
-from scraping.theguardian.client import Client as TheGuardianClient
+import os
+import logging
+import logging.handlers
+from scraping.theguardian import client as the_guardian_client
 
-API_CLIENTS = [TheGuardianClient]
+LOGGER = logging.getLogger(__name__)
+SYSLOG_HANDLER = logging.handlers.SysLogHandler(address='/dev/log')
+SYSLOG_HANDLER.setLevel(logging.INFO)
+
+API_CLIENTS = [the_guardian_client.Client]
 SOCKET_PATH = './uds_socket'
