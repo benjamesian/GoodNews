a6bae8ad9a3a4836080da47674f3f3298dd15b11
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-03-15 09:08:15 -0700

    Messing with async IO & added cleanup to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,30 +7,42 @@
 # 4. Apply configuration via puppet manifests.
 ########################################################
 
-# Set up the environment.
-
-set -o errexit      # If an error occurs, exit.
-
-if [[ -v DEBUG ]]   # Enable debugging output if `DEBUG' is defined.
+# If `DEBUG' is defined, enable debugging output.
+if [[ -v DEBUG ]]
 then
   exec {BASH_XTRACEFD}>&2
   set -o verbose -o xtrace
 fi
-if ! (( $# ))       # Set remote targets if none were supplied as arguments.
+
+# Initialize environment. If an error occurs, exit.
+set -o errexit
+
+# Specify remote targets.
+TARGETS=('35.196.167.155' '34.73.252.236')
+
+# Set max number of old archives to keep.
+if ! [[ ${ARCHIVE_MAX:=10} -ne 0 ]]
 then
-  set -- '35.196.167.155' '34.73.252.236'
-fi
+  ARCHIVE_MAX=10
+fi 2> /dev/null
+
+# Define release parameters.
 PROJECT="$(CDPATH='' cd -- "${BASH_SOURCE[0]%/*}" && pwd -P)"
-RELEASE="${PROJECT##*/}-$(date --utc '+%Y%m%d%H%M%S')"
 LOGFILE="${PROJECT}/deploy.log"
 EXCLUDE="${PROJECT}/deploy.ignore"
+RELEASE="${PROJECT##*/}-$(date --utc '+%Y%m%d%H%M%S')"
+
+# Create a temporary work directory.
 WORKDIR="$(mktemp -d --tmpdir "${BASH_SOURCE[0]##*/}-XXXXX")"
+# Remove temporary files upon exit.
 trap 'rm -rf -- "${WORKDIR}"' EXIT
-cd -- "${WORKDIR}"  # Enter temporary working directory
+# Enter temporary working directory.
+cd -- "${WORKDIR}"
 
-set +o errexit      # Environment setup complete.
+# Initialization complete.
+set +o errexit
 
-# Define a function to make a local archive of a release
+# Makes a local archive of a release
 # usage: deploy::archive
 deploy::archive()
 {
@@ -39,12 +51,11 @@ deploy::archive()
   tar -czf "${RELEASE}.tar.gz" -- "${RELEASE}"
 }
 
-# Define a function to upload a release to a single host
-# usage: deploy::upload HOST
-deploy::upload()
+# Prepares a host recieve a release
+# usage: deploy::prepare HOST
+deploy::prepare()
 {
-  ssh -T -- "ubuntu@$1"
-  scp -- "${RELEASE}.tar.gz" "ubuntu@$1:/data/releases/"
+  tee >(cat >&2) | ssh -T -- "ubuntu@$1"
 } << EOF
 sudo --non-interactive mkdir -pm 0755 /data
 sudo --non-interactive mkdir -pm 0755 /data/releases
@@ -52,51 +63,94 @@ sudo --non-interactive chown -hR ubuntu:ubuntu /data
 exit
 EOF
 
-# Define a function to install a release on a single host
+# Uploads an archived release to a host
+# usage: deploy::release HOST
+deploy::release()
+{
+  scp -- "${RELEASE}.tar.gz" "ubuntu@$1:/data/releases/"
+}
+
+# Installs an uploaded release on host
 # usage: deploy::install HOST
 deploy::install()
 {
-  ssh -T -- "ubuntu@$1"
+  tee >(cat >&2) | ssh -T -- "ubuntu@$1"
 } << EOF
-cd /data/releases
-tar -xzf '${RELEASE/\'/\'\\\'\'}.tar.gz'
-sudo --non-interactive chown -R ubuntu:ubuntu -- '${RELEASE/\'/\'\\\'\'}'
-cp -LpR -- /data/current '${RELEASE/\'/\'\\\'\'}.old'
-rm -fr /data/current
-tar -czf '${RELEASE/\'/\'\\\'\'}.old.tar.gz' -- '${RELEASE/\'/\'\\\'\'}.old'
-rm -fr -- '${RELEASE/\'/\'\\\'\'}.old'
-mkdir -pm 0755 /data/current
-cd /data/current
-ln -s '../releases/${RELEASE/\'/\'\\\'\'}'/* .
-printf '%s\0' manifests/*.pp |
-  xargs -n 1 --null --verbose sudo --non-interactive puppet apply
+if cd /data/releases
+then
+  if IFS='' read -r -d '' REPLY
+  then
+    mv -- "\${REPLY}" "\${REPLY}.backup"
+    tar -czf "\${REPLY}.backup.tar.gz" -- "\${REPLY}.backup"
+    rm -fr "\${REPLY}.backup"
+  fi < <(
+  find -H . -maxdepth 2 -type f -samefile ../current/AUTHORS -printf '%h\\0'
+  )
+  tar -xzf '${RELEASE/\'/\'\\\'\'}.tar.gz'
+  sudo --non-interactive chown -R ubuntu:ubuntu -- '${RELEASE/\'/\'\\\'\'}'
+  if cd /data/current
+  then
+    rm -rf -- {.{.?,[^.]},}*
+    ln -sv '../releases/${RELEASE/\'/\'\\\'\'}'/* .
+    printf '%s\\0' manifests/*.pp |
+      xargs --max-args=1 --null --verbose sudo --non-interactive puppet apply
+  fi
+fi
 exit
 EOF
 
-# Locally archive a current copy of the project repo
-echo 'Creating local archive'
-deploy::archive | tee -a "${LOGFILE}"
+# Removes old archives. ARCHIVE_MAX to specifies how many to keep (default: 7)
+# usage: clean HOST
+deploy::cleanup()
+{
+  (( ARCHIVE_MAX < 1 )) || tee >(cat >&2) | ssh -T -- "ubuntu@$1"
+} << EOF
+find /data/releases -name '*.tar.gz' -maxdepth 1 -printf '%Ts\t%p\0' |
+  sort --numeric-sort --zero-terminated |
+  head --lines=$(( -1 * ARCHIVE_MAX )) --zero-terminated |
+  cut --fields=2 --zero-terminated |
+  xargs --max-args=1 --max-procs=0 --null --verbose rm -f --
+EOF
 
-# Upload to hosts in parallel with a separate log for each
-echo
-for (( INDEX = 1; INDEX <= $#; ++INDEX ))
-do
-  printf 'Uploading archive to %s\n' "${!INDEX}"
-  { printf '%s: %s\n' "$(date '+%c')" "${!INDEX}"
-    (deploy::upload "${!INDEX}") &
-  } &> "$(printf "%0${##}d" "${INDEX}").logfile"
-done
-wait
-cat -- *.logfile | tee -a "${LOGFILE}"
+# Applies a function asynchronysly to mutliple hosts
+# usage: deploy::execute FUNCTION HOST ...
+deploy::execute()
+{
+  local func="$1" pids=( ) proc=0
+  shift
+  while (( ++proc <= "$#" ))
+  do
+    { printf '%s: %s\n' "$(date '+%c')" "${!proc}"
+      "${func}" "${!proc}" &
+    } &> "${proc}.log"
+    pids+=("$!")
+  done
+  proc=0
+  while (( ++proc <= "$#" ))
+  do
+    wait "${pids[proc - 1]}"
+    cat <"${proc}.log"
+  done
+}
 
-# Install on hosts in parallel with a separate log for each
+# Copy standard output and standard error to a log file
+exec {stdout}>&1 1> >(tee -a "${LOGFILE}" >&"${stdout}")
+exec {stderr}>&2 2> >(tee -a "${LOGFILE}" >&"${stderr}")
+
+# Create a local archive of the project repo
+echo 'Archiving...'
+deploy::archive
 echo
-for (( INDEX = 1; INDEX <= $#; ++INDEX ))
-do
-  printf 'Installing release on %s\n' "${!INDEX}"
-  { printf '%s: %s\n' "$(date '+%c')" "${!INDEX}"
-    (deploy::install "${!INDEX}") &
-  } &> "$(printf "%0${##}d" "${INDEX}").logfile"
-done
-wait
-cat -- *.logfile | tee -a "${LOGFILE}"
+echo 'Preparing...'
+deploy::execute deploy::prepare "${TARGETS[@]}"
+echo
+echo 'Uploading...'
+deploy::execute deploy::release "${TARGETS[@]}"
+echo
+echo 'Installing...'
+deploy::execute deploy::install "${TARGETS[@]}"
+echo
+echo 'Cleaning...'
+deploy::execute deploy::cleanup "${TARGETS[@]}"
+echo
+echo 'Done!'
