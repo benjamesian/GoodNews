4b0bdc98cf755a731003c23423962abeda710f06
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-03-19 14:33:42 -0700

    Update the guardian scraper

--- scraping/theguardian/__init__.py
+++ scraping/theguardian/__init__.py
@@ -6,7 +6,7 @@ import os
 import pathlib
 import scraping
 
-API = 'https://content.guardianapis.com'
+API_URL = 'https://content.guardianapis.com'
 AUTH_FILE = os.getenv(
     'GOOD_NEWS_THE_GUARDIAN_AUTH_FILE',
     os.path.join(os.path.dirname(__file__), 'auth.json')
@@ -15,3 +15,11 @@ DATA_FILE = os.getenv(
     'GOOD_NEWS_THE_GUARDIAN_AUTH_FILE',
     os.path.join(os.path.dirname(__file__), 'data.json')
 )
+ARTICLE_KEY_MAP = {
+    'url': 'webUrl',
+    'body': 'bodyText',
+    'title': 'webTitle',
+    'author': 'byline',
+    'created_at': 'webPublicationDate',
+    'picture_url': 'thumbnail',
+}
