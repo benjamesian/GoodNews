8071ad3a430253b46585074b4191a842deb55502
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-03-24 21:20:48 -0700

    Fixed covid-19 content filter to skip emtpy strings & None

--- scraping/filters/covid19.py
+++ scraping/filters/covid19.py
@@ -11,4 +11,6 @@ def content_filter(articles: List[dict]) -> List[dict]:
     Filters out articles relating to coronavirus
     """
     pat = re.compile(r'\b(corona.?virus|covid.?19)\b', flags=re.IGNORECASE)
-    return [a for a in articles if not any(map(pat.search, a.values()))]
+    return [a for a in articles if not any(
+        map(pat.search, filter(bool, a.values()))
+    )]
